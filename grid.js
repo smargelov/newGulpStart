0f16b472bdd9f68c2e4ad35d75f8b1f7a7d1ae43
Sergey Margelov <smargelov@gmail.com>
2019-12-13 00:24:08 +0300

    Изменены дефолтные настройки сетки

--- grid.js
+++ grid.js
@@ -6,7 +6,23 @@ var settings = {
   columns: 12,
   mobileFirst: false,
   container: {
-    maxWidth: "1170px"
+    maxWidth: "1280px",
+    fields: '30px'
+  },
+  breakPoints: {
+    lg: {
+        width: '1280px'
+    },
+    md: {
+        width: '1000px'
+    },
+    sm: {
+        width: '768px',
+        fields: '15px' 
+    },
+    xs: {
+        width: '480px'
+    }
   }
 };
 
